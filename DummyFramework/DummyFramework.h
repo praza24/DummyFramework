23d12e8de906b2316c55984d6bba285fc49f1401
praza24 <prasathsrithar@hotmail.co.uk>
2020-06-06 17:42:11 +0100

    Initial Commit

---
+++ DummyFramework/DummyFramework.h
@@ -0,0 +1,19 @@
+//
+//  DummyFramework.h
+//  DummyFramework
+//
+//  Created by Prasath Srithar on 06/06/2020.
+//  Copyright © 2020 Prasath Srithar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DummyFramework.
+FOUNDATION_EXPORT double DummyFrameworkVersionNumber;
+
+//! Project version string for DummyFramework.
+FOUNDATION_EXPORT const unsigned char DummyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummyFramework/PublicHeader.h>
+
+
